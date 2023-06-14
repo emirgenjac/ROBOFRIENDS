cb936828b54c155f92930cfa4e668cd02d132614
Emir Genjac <genjacemir5@gmail.com>
2023-06-14 20:56:40 +0200

    Implementing hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,53 +1,70 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import './App.css';
 import Scroll from '../components/Scroll';
+import ErrorBoundry from "../components/ErrorBoundry";
 
 
 
 
-class App extends React.Component  {
-    constructor() {
-        super();
-        this.state = {
-            robots: [],
-            searchfield: ''
-        }
-    }
+function App ()  {
+    //constructor() {
+    //    super();
+    //    this.state = {
+    //        robots: [],
+    //        searchfield: ''
+    //    }
+    //}
+    
+    //REACT HOOKS
+    const [ robots, setRobots ] = useState([])
+    const [ searchfield, setSearchfield ] = useState('')
+    const [ count, setCount ] = useState(0)
 
-    componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response => response.json())
-        .then(users => this.setState({robots:users}))
+    //componentDidMount() {
+    //   fetch('https://jsonplaceholder.typicode.com/users')
+    //    .then(response => response.json())
+    //    .then(users => this.setState({robots:users}))
         
-    }
+    //}
 
-    onSearchChange = (event) => {
-        this.setState({searchfield: event.target.value});
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => {setRobots(users)})
+            
+    }, [])
+
+    const onSearchChange = (event) => {
+        setSearchfield(event.target.value);
     }
     
-    render() {
-        const { robots, searchfield } = this.state;
+     
+        
         const filteredRobots = robots.filter(robot => {
             return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         })
-        
+
         return robots.lenght ? 
             <h1 className="tc">Loading...</h1>
         : 
             (
                 <div className="tc">
                 <h1 className="f1">RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
+                <button onClick={() => setCount(count+1)}> Click Me!</button>
+                
+                <SearchBox searchChange={onSearchChange}/>
                 <Scroll>
-                <CardList robots={filteredRobots}/>
+                    <ErrorBoundry>
+                        <CardList robots={filteredRobots}/>
+                    </ErrorBoundry>
                 </Scroll>
                 </div>
                 )
         }
 
-    }
+    
 
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'tachyons';
 import App from './containers/App';
-//import App from './App';
-//import reportWebVitals from './reportWebVitals';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 <App />
